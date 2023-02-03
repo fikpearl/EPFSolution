192c32cfe2aa92228c0807215569cbff316f3e8f
fikpearl021 <fikpearl021@student.wethinkcode.co.za>
2023-02-03 15:15:25 +0200

    Server in progress

--- src/main/java/org/example/MyServer.java
+++ src/main/java/org/example/MyServer.java
@@ -5,10 +5,29 @@ import io.javalin.http.staticfiles.Location;
 
 
 public class MyServer {
+
+    private static final String PAGES_PATH = "/public";
+
+    public MyServer() {
+        Javalin server1 = Javalin.create(config -> config.addStaticFiles(PAGES_PATH, Location.CLASSPATH));
+    }
+
     public static void main(String[] args) {
 
-
+        MyServer server = new MyServer();
+        server.start(5000);
 
         System.out.println("Hello world!");
+    }
+
+    private void start(int port) {
+    }
+
+    private void stop(){
+
+    }
+
+    private void port(){
+
     }
 }
