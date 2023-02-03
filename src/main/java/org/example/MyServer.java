e6ff376fdeddefb565f1e7c43fd3f2a1420370bf
fikpearl021 <fikpearl021@student.wethinkcode.co.za>
2023-02-03 14:52:18 +0200

    Fix pom file to manage dependencies

--- src/main/java/org/example/MyServer.java
+++ src/main/java/org/example/MyServer.java
@@ -1,7 +1,13 @@
 package org.example;
 
-public class Main {
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
+
+public class MyServer {
     public static void main(String[] args) {
+
+
         System.out.println("Hello world!");
     }
 }
