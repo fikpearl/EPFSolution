d1ab50e388af73d2fd58fec4ea11bbf68e23ed68
fikpearl021 <fikpearl021@student.wethinkcode.co.za>
2023-02-08 11:15:00 +0200

    financial records uploaded,transaction history uploaded,grapg added,buttons functionality added

--- src/main/resources/path/js/app.js
+++ src/main/resources/path/js/app.js
@@ -1,21 +1,28 @@
-//collecting the data from our html form
-let form = document.getElementById('form')
 
-form.addEventListener('submit',(e) => {
-    e.preventDefault();
+function ourformdata(){
+
+document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const fnfield = document.querySelector('firstNameField').value = "Pearl";
+    const lnfield = document.querySelector('LastNameField').value = "Gqibitole";
+    const dofb = document.querySelector('DateOfBirthField').value = "02/06/20203";
+});
+
+    /*fetch("https://github.com/fikpearl/EPFSolution/blob/2eca461608f4947d49f192428566a3d3eae9564b/Data/Financial%20Records.xlsx", {
+      method: "POST",
+      body: JSON.stringify(fnfield,lnfield,dofb)})
 })
+    
+    .then((data) => data.json())
+    .then((data) => {*/
+      
+function verifyName(){
 
-let fnfield = document.getElementById('firstNameField').value;
-
-let lnfield = document.getElementById('LastNameField').value;
-
-let dofb = document.getElementById('DateOfBirthField').value;
-
-
-
-
-
-
+    if(fnfield.value == null && lnfield.value == null && dofb.value == null){
+        document.getElementById("submit_button").disabled = true;
+    }
+    else {
         
-
-     
+        document.getElementById("submit_button").disabled = false;
+}
