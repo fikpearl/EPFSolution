2e7fdce3c88ae842ed0872d1c1b0794180ed20f7
fikpearl021 <fikpearl021@student.wethinkcode.co.za>
2023-02-10 19:02:53 +0200

    Adding of Comments since loadshedding delayed

--- src/main/java/org/example/MyServer.java
+++ src/main/java/org/example/MyServer.java
@@ -3,12 +3,20 @@ package org.example;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 
+//This is my Java server class that starts the Javalin server on port 5000
+//to the console when it is started.
+//The code starts the server on port 5000.
 
 public class MyServer {
 
     private static final String PAGES_PATH = "/path";
 
     private final Javalin server1;
+
+
+    //We create a server called "MyServer" and assign it the configuration object.
+    //The config object is then used to add static files to the classpath, which are located in the directory "PAGES_PATH".
+    //We create a server instance with the name "MyServer" and listen on port 8080
 
     public MyServer() {
         server1 = Javalin.create(config -> config.addStaticFiles(PAGES_PATH, Location.CLASSPATH));
@@ -18,7 +26,7 @@ public class MyServer {
         MyServer server = new MyServer();
         server.start();
 
-        System.out.println("Hello world!");
+
     }
 
     private void start() {
--- src/main/resources/path/js/app.js
+++ src/main/resources/path/js/app.js
@@ -1,3 +1,15 @@
+/* here we have code  that starts by adding an event listener to the form.
+    The submit event is triggered when a user submits their information into the form.
+    Then, it sets up two variables: fnfield and lnfield.
+    These are set to "Pearl" and "Gqibitole", respectively.
+    Next, it creates a DateOfBirthField variable called dofb with the value of 02/06/20203.
+
+    This function has one parameter: event which is passed in from the submit event that was just created on line 2 of this code snippet (event).
+    The function starts by preventing any default behavior for the submit event so that no error messages pop up or anything like that happens when someone submits their information into this form (line 4).
+    It then adds an EventListener to the firstNameField field with its addEventListener method (line 5) and assigns it a callback function named handleSubmit(event) where we assign some logic for handling what happens when someone fills out this field (lines 6-7).
+    Next, we create another EventListener on LastNameField with its addEventListener method and assign it a callback
+    The code prevents the form from submitting when the user clicks on submit.
+.*/
 
 function ourformdata(){
 
