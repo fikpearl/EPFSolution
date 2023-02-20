f613b51084d9abdba44910a20ea3dc42e7b740b6
fikpearl021 <fikpearl021@student.wethinkcode.co.za>
2023-02-20 02:20:27 +0200

    checked and updated accordingly

--- src/main/resources/path/js/app.js
+++ src/main/resources/path/js/app.js
@@ -19,22 +19,13 @@ document.querySelector("form").addEventListener("submit", (event) => {
     const fnfield = document.querySelector('firstNameField').value = "Pearl";
     const lnfield = document.querySelector('LastNameField').value = "Gqibitole";
     const dofb = document.querySelector('DateOfBirthField').value = "02/06/20203";
-});
-
-    /*fetch("https://github.com/fikpearl/EPFSolution/blob/2eca461608f4947d49f192428566a3d3eae9564b/Data/Financial%20Records.xlsx", {
-      method: "POST",
-      body: JSON.stringify(fnfield,lnfield,dofb)})
-})
-    
-    .then((data) => data.json())
-    .then((data) => {*/
-      
-function verifyName(){
 
     if(fnfield.value == null && lnfield.value == null && dofb.value == null){
-        document.getElementById("submit_button").disabled = true;
+        return document.getElementById("submit_button").disabled = true;
     }
     else {
         
-        document.getElementById("submit_button").disabled = false;
+        return document.getElementById("submit_button").disabled = false;
+    }
 }
+
